d128f3d92248d933c9cc4fb3215d90dfa91eb29f
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-02-08 07:53:14 -0300

    feat: adicionado DTO de criaçao de jogadores

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogador() {
-    return JSON.stringify({
-      Nome: 'Leonardo',
-    });
+  async criarAtualizarJogador(@Body() criarJogadorDTO: CriarJogadorDto) {
+    const { email } = criarJogadorDTO;
+    return JSON.stringify(`{
+      "nome": ${email},
+    }`);
   }
 }
